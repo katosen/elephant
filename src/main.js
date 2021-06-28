31ba8b8f31259d4fb57491c70e3dc9436eb3b837
hc_zhang <hc_zhang@trip.com>
2021-06-28 10:03:35 +0800

    add element-plus

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
+
+const app = createApp(App)
+
+app.use(ElementPlus, { locale })
+
+app.mount('#app')
