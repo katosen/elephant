6b9487f723a7fc6606cbbcc0326fc7b0c00dafa6
harriszhang <harriszhang@live.cn>
2022-03-13 09:25:35 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
-import locale from 'element-plus/lib/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
+import locale from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 
